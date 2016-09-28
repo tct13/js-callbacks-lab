cb291d7fad97d0f94042c8660369f47cf91670df
Braden CT Tan <tct13@yahoo.com>
2016-09-28 20:02:09 +0800

    start timer done

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,99 @@
 // your code here:
+
+var timer = document.querySelector("#timer");
+var seconds = 0;
+var timerId = 0;
+
+
+var startButton = document.querySelector("#start");
+startButton.addEventListener("click", start)
+
+var resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", reset)
+
+var pauseButton = document.querySelector("#pause");
+pauseButton.addEventListener("click", pause)
+
+
+
+function start() {
+  var idValue = window.setInterval(updateTime, 1000);
+
+}
+
+
+
+
+function reset() {
+
+  window.clearInterval( idValue );
+//??? how to use the IDvalue here?????
+
+
+
+timer.textContent = "Stop Watch";
+
+
+
+}
+
+
+
+
+function pause() {
+  timer.textContent = "Time elapsed: 1";
+
+  window.clearInterval()
+//??? stop
+
+}
+
+
+function updateTime() {
+
+  seconds ++;
+  timer.textContent = "Time elapsed: " + seconds + " seconds";
+
+}
+
+
+
+
+
+
+
+// function init () {
+//
+// var body = document.querySelector("body");
+// var h2 = document.createElement("h2");
+// var i = 0;
+//
+// body.appendChild(h2);
+//
+//
+//
+// function createH1 () {
+//   var h1 = document.createElement("h1");
+//   h1.textContent = "Hello World";
+//   body.appendChild(h1);
+// //   console.log(h1.textContent);
+//
+//   window.setInterval(secCount, 1000);
+// }
+//
+//
+//
+// function secCount () {
+//   h2.textContent = "WDI " + i;
+//   i++;
+// //   console.log("WDI " + h2);
+// }
+//
+//
+// window.setTimeout(createH1, 5000);
+//
+//
+// }
+//
+//
+// init();

b90831702fd2e76fc769aa42506cce000575e9e9
Braden CT Tan <tct13@yahoo.com>
2016-09-29 09:28:02 +0800

    stablisied files

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -17,83 +17,24 @@ pauseButton.addEventListener("click", pause)
 
 
 function start() {
-  var idValue = window.setInterval(updateTime, 1000);
-
+  timerId = window.setInterval(updateTime, 1000);
 }
-
-
 
 
 function reset() {
-
-  window.clearInterval( idValue );
-//??? how to use the IDvalue here?????
-
-
-
-timer.textContent = "Stop Watch";
-
-
-
+  window.clearInterval( timerId );
+  seconds = 0;
+  timer.textContent = "Stop Watch";
 }
 
 
-
-
 function pause() {
-  timer.textContent = "Time elapsed: 1";
-
-  window.clearInterval()
-//??? stop
-
+  window.clearInterval( timerId );
+  timer.textContent = "Time elapsed: " + seconds + " seconds";
 }
 
 
 function updateTime() {
-
   seconds ++;
   timer.textContent = "Time elapsed: " + seconds + " seconds";
-
 }
-
-
-
-
-
-
-
-// function init () {
-//
-// var body = document.querySelector("body");
-// var h2 = document.createElement("h2");
-// var i = 0;
-//
-// body.appendChild(h2);
-//
-//
-//
-// function createH1 () {
-//   var h1 = document.createElement("h1");
-//   h1.textContent = "Hello World";
-//   body.appendChild(h1);
-// //   console.log(h1.textContent);
-//
-//   window.setInterval(secCount, 1000);
-// }
-//
-//
-//
-// function secCount () {
-//   h2.textContent = "WDI " + i;
-//   i++;
-// //   console.log("WDI " + h2);
-// }
-//
-//
-// window.setTimeout(createH1, 5000);
-//
-//
-// }
-//
-//
-// init();
